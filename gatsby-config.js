8fbf7eb27c3b2176149e85c0d55a58579a64337a
Paul Galow <paul.galow@gmail.com>
2019-06-17 20:10:24 +0200

    Customize gatsby-plugin-manifest config

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,8 +52,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Paul Galow`,
+        short_name: `paulgalow.com`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
