f93a1825dc5ea431a342328a744e6a3d0bb92411
Paul Galow <paulgalow@users.noreply.github.com>
2023-05-18 13:12:45 +0200

    Refactor deprecated queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -67,7 +67,7 @@ export const pageQuery = graphql`
         description
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
       edges {
         node {
           excerpt
